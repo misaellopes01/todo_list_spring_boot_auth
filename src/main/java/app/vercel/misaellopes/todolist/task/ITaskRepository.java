703dc06a28b69e38b6893022d1887d4fda555b5c
Misael Lopes <mecl.ely@gmail.com>
2023-10-15 18:54:59 +0100

    get all todo by user

--- src/main/java/app/vercel/misaellopes/todolist/task/ITaskRepository.java
+++ src/main/java/app/vercel/misaellopes/todolist/task/ITaskRepository.java
@@ -3,7 +3,9 @@ package app.vercel.misaellopes.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-  
+  List<TaskModel> findByUserID(UUID userID);
 }
--- src/main/java/app/vercel/misaellopes/todolist/task/TaskController.java
+++ src/main/java/app/vercel/misaellopes/todolist/task/TaskController.java
@@ -1,8 +1,13 @@
 package app.vercel.misaellopes.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +23,27 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel,  HttpServletRequest request){
+  public ResponseEntity create(@RequestBody TaskModel taskModel,  HttpServletRequest request){
     taskModel.setUserID((UUID) request.getAttribute("userID"));
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start or end date must be greater than currente date!");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start date must be less than end date!");
+    }
+
     var task = this.taskRepository.save(taskModel);
 
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userID = request.getAttribute("userID");
+    var tasks = this.taskRepository.findByUserID((UUID) userID);
+    return tasks;
   }
 }
