5ce6b50081fa8e7167592e937a636ec68165df3a
Misael Lopes <mecl.ely@gmail.com>
2023-10-15 20:13:35 +0100

    update complete or partial todo properties

--- src/main/java/app/vercel/misaellopes/todolist/task/TaskController.java
+++ src/main/java/app/vercel/misaellopes/todolist/task/TaskController.java
@@ -8,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.vercel.misaellopes.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -46,4 +50,16 @@ public class TaskController {
     var tasks = this.taskRepository.findByUserID((UUID) userID);
     return tasks;
   }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
+  }
+
+  
 }
